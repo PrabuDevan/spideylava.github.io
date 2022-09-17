19997bd52970d48042bed32c204ea5939cc84ec0
Amuthan Thennavan <amuthan@playtonia.com>
2022-09-18 01:24:31 +0530

    change

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,12 +1,24 @@
 
 
 
-$(function() {
-  $(window).on("scroll", function() {
-      if($(window).scrollTop() > 50) {
-          $(".alk").addClass("active");
-      } else {
-         $(".alk").removeClass("active");
-      }
-  });
-});
+// $(function() {
+//   $(window).on("scroll", function() {
+//       if($(window).scrollTop() > 50) {
+//           $(".alk").addClass("active");
+//       } else {
+//          $(".alk").removeClass("active");
+//       }
+//   });
+// });
+
+window.onscroll = function() {scrollFunction()};
+ 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    
+    document.getElementById("navbar").style.background = "#5F2314";
+  } else {
+   
+    document.getElementById("navbar").style.background = "none";
+  }
+}
